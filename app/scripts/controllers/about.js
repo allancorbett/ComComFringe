b0247b1f7822f9e28786fa88942677b9731c821f
Allan Corbett <allancorbett@gmail.com>
2016-07-26 16:10:18 +0100

    Init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comcompodOnTheFringeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comcompodOnTheFringeApp
+ */
+angular.module('comcompodOnTheFringeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comcompodOnTheFringeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comcompodOnTheFringeApp
+ */
+angular.module('comcompodOnTheFringeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    
+  });
